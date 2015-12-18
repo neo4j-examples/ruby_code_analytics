9dff5d6247d9df719dfa54a3efbe8a2924c1b3eb
Brian Underwood <ml+github@semi-sentient.com>
2015-12-18 16:04:15 +0700

    Production DB based on environment var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.neo4j.session_type = :server_db
+  config.neo4j.session_path = ENV['NEO4J_URL']
 end
