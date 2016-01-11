5d5e5577beb8175c94eed5a1808b83bce05cc8d2
Brian Underwood <ml+github@semi-sentient.com>
2016-01-11 17:37:51 +0700

    LIMIT 1 so that it is much faster

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @first_trace_point = TracePointEntry.as(:tp).where_not("()-[:NEXT]->(tp)").pluck(:tp).first
+    @first_trace_point = TracePointEntry.as(:tp).where_not("()-[:NEXT]->(tp)").limit(1).pluck(:tp).first
   end
 end
